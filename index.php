22d4ac5a337b5192ee0314af6299efa379aa7ed6
Antonio Carlos <acdona@hotmail.com>
2023-06-05 10:15:53 -0300

    feat(index.php): Enabling all error types in PHP

--- index.php
+++ index.php
@@ -2,4 +2,9 @@
  
 declare(strict_types=1);
  
-echo "Carregou a página index.php";
+// Ativa todos os tipo de erros e aviso do php
+// Deve ser retirado no deploy do projeto
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
